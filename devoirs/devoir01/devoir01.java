35d545576d3727ee9fe4924e1d32266c96651484
corpataux-cesar-emf <cesar.corpataux@studentfr.ch>
2024-08-29 12:16:33 +0200

    #test

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -2,6 +2,15 @@ package devoirs.devoir01;
 
 public class devoir01 {
     public static void main(String[] args) {
+        int monAge=16;
+        String estMajeuString;
+
+
+
+
+
+
+
         
     }
 }
