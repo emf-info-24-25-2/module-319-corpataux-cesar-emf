75b111fabc1d98357dab5bbadfa8104a0ed082af
corpataux-cesar-emf <cesar.corpataux@studentfr.ch>
2024-09-08 17:00:03 +0200

    devoire01

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -2,18 +2,21 @@ package devoirs.devoir01;
 
 public class devoir01 {
     public static void main(String[] args) {
-        int monAge=16;
+        byte monAge = 19;
         boolean estMajeur;
-        if (monAge > 18) { System.out.println();
-            
+        if (monAge < 18) {
+            estMajeur = false;
+        } else {
+            estMajeur = true;
         }
 
+        System.out.println("je m'appelle CORPATAUX César");
+        System.out.println("Mon âge est de " + monAge + " ans");
 
-
-
-
-
-
-        
+        if (estMajeur == true) {
+            System.out.println("Je suis majeur");
+        } else if (estMajeur == false) {
+            System.out.println("Je ne suis pas encore majeur");
+        }
     }
 }
