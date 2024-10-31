8b74e5b660e3589def98306e443d9af06f3b08c6
corpataux-cesar-emf <cesar.corpataux@studentfr.ch>
2024-10-31 09:55:00 +0100

    exercice10

---
+++ exercices/exercice10/Methodes1.java
@@ -0,0 +1,61 @@
+public class Methodes1 {
+    public static void direBonjour() {
+        System.out.println("Bonjour");
+    }
+    public static void dire(String dire){
+        System.out.println(dire);
+    }
+
+
+    public static void main(String[] args) {
+        direBonjour();
+        dire("salut tout le monde");
+        dire("hello james");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
---
+++ exercices/exercice10/Methodes2.java
@@ -0,0 +1,5 @@
+public class Methodes2 {
+    public void dire(String[] args){
+
+    }
+}
