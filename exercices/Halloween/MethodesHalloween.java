e94b8a0575be30acb82cf25338c57fac07a1a980
corpataux-cesar-emf <cesar.corpataux@studentfr.ch>
2024-11-07 08:18:53 +0100

    #

---
+++ exercices/Halloween/MethodesHalloween.java
@@ -0,0 +1,11 @@
+public class MethodesHalloween {
+
+    public static void halloween(String message) {
+        System.out.println("Boo");
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        halloween("Joyeux Halloween !");
+    }
+}
--- exercices/exercice10/Methodes1.java
+++ exercices/exercice10/Methodes1.java
@@ -2,60 +2,57 @@ public class Methodes1 {
     public static void direBonjour() {
         System.out.println("Bonjour");
     }
-    public static void dire(String dire){
+
+    public static void dire(String dire) {
         System.out.println(dire);
     }
+    public static void Math(){
+        int Min=Math.min(1, 2);
+        int Max=Math.max(10, 25);
+        double Pow=Math.pow(10, 25);
+        double Sqrt=Math.sqrt(25);
+        int Abs=Math.abs(-10);
 
+        System.out.println(Min);
+        System.out.println(Max);
+        System.out.println(Pow);
+        System.out.println(Sqrt);
+        System.out.println(Abs);
+    
+    }
 
     public static void main(String[] args) {
         direBonjour();
         dire("salut tout le monde");
         dire("hello james");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Math();
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-
