21d588ba095808005ddc30dea59e20fbb3dad6be
corpataux-cesar-emf <cesar.corpataux@studentfr.ch>
2024-11-07 09:09:22 +0100

    Exercice7

---
+++ exercices/exercice07/OperateursJava.java
@@ -0,0 +1,74 @@
+public class OperateursJava {
+
+    public static void main(String[] args) {
+
+        // on utilise le signe “+” pour additionner deux variable.
+        System.out.println("le résulat est "+(30+17));
+    // Expected output: 47
+
+
+        // La Soustraction, Soustrait le nombre de droite de celui de gauche.
+        System.out.println("le contenu de la variable 1 est " + (41-20));
+        // Expected output: 21
+
+
+        // La multiplication, multiplie deux nombres.
+        System.out.println("le résultat de multiplication est "+(20*5));
+        // Expected output: 100
+
+
+        // La Division, / Divise le nombre de gauche par celui de droite.
+        System.out.println("le résultat de la division est "+ (41/5));
+        // Expected output: 8
+
+
+        // Modulo, Renvoie le reste de la division du nombre de gauche par celui de droite. ( en gros ce qui reste de la division)
+        System.out.println("le modulo est de "+(5 % 20));
+        // Expected output: 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
